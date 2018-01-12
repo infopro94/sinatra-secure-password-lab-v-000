679a7d8a26946a9f2e3fb725c5c86fa4e42316d2
Susan Fowler <infopro94@gmail.com>
2018-01-12 19:59:49 +0000

    automatically backed up by learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -19,7 +19,7 @@ class ApplicationController < Sinatra::Base
 
   post "/signup" do
     if params[:username] == "" || params[:password] == ""
-      redirect '/failure'
+      redirect "/failure"
     else
       User.create(username: params[:username], password: params[:password])
       redirect "/login"
